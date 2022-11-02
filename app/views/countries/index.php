1dece60e2577d4e9a3a81e72fc6797bd77e45c0f
Berkant Ates <321906@student.mboutrecht.nl>
2022-11-02 14:45:05 +0100

    Added views files

---
+++ app/views/countries/index.php
@@ -0,0 +1,25 @@
+<ul>
+    <li>
+        <a href="<?= URLROOT; ?>/landingpages/index">Ga terug naar de hoofdpagina!</a>
+    </li>
+</ul>
+
+<h1><?= $data['title'] ?> <br></h1>
+<h4><?= $data['description']?></h4>
+
+
+<table>
+    <thead>
+        <th>Id</th>
+        <th>Naam</th>
+        <th>Vermogen</th>
+        <th>Leeftijd</th>
+        <th>Bedrijf</th>
+        <th>Delete</th>
+    </thead>
+    <tbody>
+        <?= $data['rows']; ?>
+    </tbody>
+</table>
+
+<h1><?= $data['onzin']?></h1>
---
+++ app/views/users/index.php
@@ -0,0 +1,23 @@
+<ul>
+    <li>
+        <a href="<?= URLROOT; ?>/landingpages/index">Ga terug naar de hoofdpagina!</a>
+    </li>
+</ul>
+
+<h1><?= $data['title'] ?> <br></h1>
+<h4><?= $data['description']?></h4>
+
+<table>
+    <thead>
+        <th>Id</th>
+        <th>Naam</th>
+        <th>Vermogen</th>
+        <th>Leeftijd</th>
+        <th>Bedrijf</th>
+    </thead>
+    <tbody>
+        <?= $data['rows']; ?>
+    </tbody>
+</table>
+
+<h1><?= $data['privacy']?></h1>
